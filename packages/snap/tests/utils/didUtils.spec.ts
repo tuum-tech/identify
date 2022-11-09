578fc21287cf2b6d9d652d5b7d4ed6bc0b409359
Kiran Pachhai <kiran.pachhai@gmail.com>
2022-11-09 15:24:38 -0500

    Added more tests

---
+++ packages/snap/tests/utils/didUtils.spec.ts
@@ -0,0 +1,40 @@
+import { SnapProvider } from '@metamask/snap-types';
+import { getCurrentDid } from '../../src/utils/didUtils';
+import {
+  address,
+  exampleDIDPkh,
+  getDefaultSnapState,
+} from '../testUtils/constants';
+import { createMockWallet, WalletMock } from '../testUtils/wallet.mock';
+
+describe('Utils [did]', () => {
+  let walletMock: SnapProvider & WalletMock;
+
+  beforeEach(() => {
+    walletMock = createMockWallet();
+  });
+
+  describe('getCurrentDid', () => {
+    it('should return did:pkh', async () => {
+      const initialState = getDefaultSnapState();
+
+      await expect(
+        getCurrentDid(walletMock, initialState, address)
+      ).resolves.toBe(`did:pkh:eip155:0x4:${address}`);
+
+      expect.assertions(1);
+    });
+
+    it('should return did:key', async () => {
+      const initialState = getDefaultSnapState();
+      initialState.accountState[address].accountConfig.identity.didMethod =
+        'did:pkh';
+
+      await expect(
+        getCurrentDid(walletMock, initialState, address)
+      ).resolves.toBe(exampleDIDPkh);
+
+      expect.assertions(1);
+    });
+  });
+});
---
+++ packages/snap/tests/utils/init.spec.ts
@@ -0,0 +1,35 @@
+import { SnapProvider } from '@metamask/snap-types';
+import { getInitialSnapState } from '../../src/utils/config';
+import { init } from '../../src/utils/init';
+import { createMockWallet, WalletMock } from '../testUtils/wallet.mock';
+
+describe('RPC handler [init]', () => {
+  let walletMock: SnapProvider & WalletMock;
+
+  beforeEach(() => {
+    walletMock = createMockWallet();
+  });
+
+  it('should succeed for accepted terms and conditions', async () => {
+    const initialState = getInitialSnapState();
+    walletMock.rpcMocks.snap_confirm.mockReturnValueOnce(true);
+
+    await expect(init(walletMock)).resolves.toEqual(initialState);
+    expect(walletMock.rpcMocks.snap_manageState).toHaveBeenCalledWith(
+      'update',
+      initialState
+    );
+
+    expect.assertions(2);
+  });
+
+  it('should fail for rejected terms and conditions', async function () {
+    walletMock.rpcMocks.snap_confirm.mockReturnValueOnce(false);
+
+    await expect(init(walletMock)).rejects.toThrow(
+      new Error('User did not accept terms and conditions!')
+    );
+
+    expect.assertions(1);
+  });
+});
