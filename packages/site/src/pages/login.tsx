5ebad98d5ac71dab7b2aca71b0174a9e80846c5c
dchagastelles <dchagastelles@gmail.com>
2023-01-27 16:06:42 -0500

    - adjustements and corrections

--- packages/site/src/pages/login.tsx
+++ packages/site/src/pages/login.tsx
@@ -1,72 +1,102 @@
 import axios from 'axios';
-import { useState } from 'react';
-import { createVP, getCurrentNetwork, getVCs, saveVC } from './../utils/snap';
+import { useEffect, useState } from 'react';
+import { createVP, getCurrentNetwork, getDID, getVCs, saveVC } from './../utils/snap';
 
 import { ProofInfo } from '@tuum-tech/identity-snap/src/types/params';
 import { IDataManagerQueryResult } from '@tuum-tech/identity-snap/src/veramo/plugins/verfiable-creds-manager';
 import { VerifiableCredential, VerifiablePresentation } from '@veramo/core';
+import { Button, Modal } from 'react-bootstrap';
 import {
   Card, SendHelloButton
 } from '../components';
 
-// dotenv.config();
 
 function LoginPage() {
   const [loginName, setLoginName] = useState('dchagastelles');
 
   // TODO: get did by calling getDid
-  const [identifier, setIdentifier] = useState(
-    'did:pkh:eip155:296:0x7d871f006d97498ea338268a956af94ab2e65cdd',
-  );
+  const [identifier, setIdentifier] = useState(''); //useState('did:pkh:eip155:296:0x7d871f006d97498ea338268a956af94ab2e65cdd');
   const [currentChainId, setCurrentChainId] = useState('');
-  const [presentation, setPresentation] = useState({});
+  const [presentation, setPresentation] = useState<VerifiablePresentation | undefined>(undefined);
 
   const [vc, setVC] = useState('');
-  const [vcId, setVcId] = useState('138af73c-f22d-4c75-b578-773fc70dff28');
+  const [vcId, setVcId] = useState('');
+  const [vcList, setVcList] = useState([] as any);
+  const [showVcsModal, setShowVcsModal] = useState(false);
+
+
+  useEffect(()=>{
+    (async () => {
+      
+      await handleSaveVC();
+
+    })();
+  }, [vc]);
+
+   useEffect(()=>{
+    (async () => {
+      if (presentation !== undefined){
+
+        const backend_url = process.env.GATSBY_BACKEND_URL;
+        const ret = await axios({
+          method: 'post',
+          url: `${backend_url}api/v1/credential/signin`,
+          data: {
+            presentation
+          },
+        });
+        alert("Verified: " + JSON.stringify(ret.data));
+      }
+    })();
+  }, [presentation]);
+
+  useEffect(()=>{
+    (async () => {
+      
+      if (identifier !== ''){
+
+        // Send a POST request to obtain a signed VC from the backend
+        const backend_url = process.env.GATSBY_BACKEND_URL;
+        const ret = await axios({
+          method: 'post',
+          url: `${backend_url}api/v1/credential/register`,
+          data: {
+            loginName,
+            identifier,
+          },
+        });
+  
+        setVC(JSON.stringify(ret.data));
+      }
+      })();
+  }, [identifier]);
 
   const handleCreateVC = async () => {
-    // Send a POST request
 
-    const backend_url = process.env.GATSBY_BACKEND_URL;
-    const ret = await axios({
-      method: 'post',
-      url: `${backend_url}api/v1/credential/register`,
-      data: {
-        loginName,
-        identifier,
-      },
-    });
+    // Get the current did
 
-    setVC(JSON.stringify(ret.data));
-    
+    setIdentifier(await getDID() as string);
+
+
+  
   };
 
 
   const handleSaveVC = async () => {
     // Send a POST request
+    if (vc !== ''){
 
-    let parsedVC : VerifiableCredential = JSON.parse(vc) as VerifiableCredential;
-    await saveVC(parsedVC);
+      let parsedVC : VerifiableCredential = JSON.parse(vc) as VerifiableCredential;
+      await saveVC(parsedVC);
+      
+    };
     
   };
 
-  const handleSignIn = async () => {
-    // Send a POST request
+ 
 
-     const backend_url = process.env.GATSBY_BACKEND_URL;
-    const ret = await axios({
-      method: 'post',
-      url: `${backend_url}api/v1/credential/signin`,
-      data: {
-        presentation
-      },
-    });
-
-    setVC(JSON.stringify(ret.data));
-  };
-
-  const handleCreateVPClick = async () => {
-     try {
+  const handleVCClicked = async (vcId: string) => {
+    try {
       setCurrentChainId(await getCurrentNetwork());
       const proofInfo: ProofInfo = {
         proofFormat: 'jwt',
@@ -74,20 +104,20 @@ function LoginPage() {
       };
       console.log('vcId: ', vcId);
       const vp = (await createVP(
-        vcId.trim().split(','),
+        [vcId],
         proofInfo
       )) as VerifiablePresentation;
       setPresentation(vp);
       console.log(`Your VP is: ${JSON.stringify(vp, null, 4)}`);
-      alert(`Your VP is: ${JSON.stringify(vp, null, 4)}`);
+      setShowVcsModal(false);
     } catch (e) {
       console.error(e);
       //dispatch({ type: MetamaskActions.SetError, payload: e });
     }
-  }
+  } 
 
 
-  const handleGetVCsClick = async () => {
+  const handleSignIn = async () => {
     try {
       setCurrentChainId(await getCurrentNetwork());
       const options = {
@@ -98,15 +128,16 @@ function LoginPage() {
         undefined,
         options
       )) as IDataManagerQueryResult[];
+
       console.log(`Your VCs are: ${JSON.stringify(vcs, null, 4)}`);
       if (vcs.length > 0) {
-        const keys = vcs.map((vc: { metadata: any }) => vc.metadata.id);
-        if (keys.length > 0) {
-          // setVcId(keys.toString());
-          // setVcIdsToBeRemoved(keys.toString());
-          // setVc(vcs[keys.length - 1].data as IDataManagerQueryResult);
-          alert(`Your VC IDs are: ${keys.toString()}`);
-        }
+       
+          setVcList(vcs);
+          setShowVcsModal(true);
+          
+       
+      } else {
+        // no need to select
       }
     } catch (e) {
       console.error(e);
@@ -116,6 +147,23 @@ function LoginPage() {
 
   return (
     <>
+     <Modal show={showVcsModal} onHide={() => setShowVcsModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Choose the credential to Sign in with</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {vcList.map((cred: any) => (
+            <div>
+              <span onClick={() => handleVCClicked(cred.metadata.id)}>{`Login : ${cred.data.credentialSubject.loginName}  Issuance: ${cred.data.issuanceDate}`}</span>
+            </div>
+          ))}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => setShowVcsModal(false)}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <Card
         content={{
           title: 'createVC',
@@ -130,15 +178,7 @@ function LoginPage() {
                   onChange={(e) => setLoginName(e.target.value)}
                 />
               </label>
-              <br />
-              <label>
-                Enter your did
-                <input
-                  type="text"
-                  value={identifier}
-                  onChange={(e) => setIdentifier(e.target.value)}
-                />
-              </label>
+             
             </form>
           ),
           button: (
@@ -151,42 +191,12 @@ function LoginPage() {
         }}
         disabled={false}
       />
-      <p>
-        {vc} 
-        <SendHelloButton
-          buttonText="Save VC"
-          onClick={handleSaveVC}
-          disabled={false}
-        />
-        <SendHelloButton
-          buttonText="Get VCs"
-          onClick={handleGetVCsClick}
-          disabled={false}
-        />
-        <SendHelloButton
-          buttonText="Generate Presentation"
-          onClick={handleCreateVPClick}
-          disabled={false}
-        />
-
-      </p>
+      
             
       <Card
         content={{
           title: 'Sign In',
           description: 'Present VerifiableCredential so we could verify',
-          form: (
-            <form>
-              <label>
-                Enter your VerifiablePresentation
-                <input
-                  type="text"
-                  value={JSON.stringify(presentation)}
-                  onChange={(e) => setPresentation(e.target.value)}
-                />
-              </label>
-            </form>
-          ),
           button: (
             <SendHelloButton
               buttonText="SignIn"
