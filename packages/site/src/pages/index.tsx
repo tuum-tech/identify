fd03af26c219a6406f04813f14b4892b1a99ac5f
luckdev01 <luckdev01@gmail.com>
2023-02-09 14:15:06 -0500

    Remove todo

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -39,8 +39,8 @@ import {
   resolveDID,
   sendHello,
   shouldDisplayReconnectButton,
-  togglePopups,
   syncGoogleVCs,
+  togglePopups,
   uploadToGoogleDrive,
   verifyVC,
   verifyVP,
@@ -972,43 +972,6 @@ const Index = () => {
                 <SendHelloButton
                   buttonText="Sync Google VCs"
                   onClick={handleSyncGoogleVCs}
-                  disabled={!state.installedSnap}
-                />
-              ),
-            }}
-            disabled={!state.installedSnap}
-            fullWidth={
-              state.isFlask &&
-              Boolean(state.installedSnap) &&
-              !shouldDisplayReconnectButton(state.installedSnap)
-            }
-          />
-        ) : (
-          ''
-        )}
-        {/* =============================================================================== */}
-        {(validHederaChainID(currentChainId) && hederaAccountConnected) ||
-        (!validHederaChainID(currentChainId) && !hederaAccountConnected) ? (
-          <Card
-            content={{
-              title: 'todo',
-              description: 'TODO',
-              /* form: (
-              <form>
-                <label>
-                  Enter your Verifiable Presentation
-                  <input
-                    type="text"
-                    value={JSON.stringify(vp)}
-                    onChange={(e) => setVp(e.target.value)}
-                  />
-                </label>
-              </form>
-            ), */
-              button: (
-                <SendHelloButton
-                  buttonText="todo"
-                  onClick={handleVerifyVPClick}
                   disabled={!state.installedSnap}
                 />
               ),
