6a5bb4cf1767b2cc1c0dd77b0a66c72f51f1772b
dchagastelles <dchagastelles@gmail.com>
2023-02-19 14:36:56 -0500

    Add new tests

---
+++ packages/snap/tests/rpc/getAvailableMethods.spec.ts
@@ -0,0 +1,31 @@
+import { getAvailableMethods } from '../../src/rpc/did/getAvailableMethods';
+
+
+jest.mock('uuid');
+
+  describe('GetAvailableMethods', () => {
+    
+
+    it('should return all available methods', async () => {
+
+      // get  
+      let getAvailableMethodsResult = await getAvailableMethods();
+      expect(getAvailableMethodsResult.length).toBeGreaterThanOrEqual(1);
+    
+      expect.assertions(1);
+    });
+
+
+    it('should contains did:pkh in available methods', async () => {
+
+      // get  
+      let getAvailableMethodsResult = await getAvailableMethods();
+      expect(getAvailableMethodsResult).toContain("did:pkh");
+    
+      expect.assertions(1);
+    });
+
+
+
+
+});
---
+++ packages/snap/tests/rpc/switchMethod.spec.ts
@@ -0,0 +1,69 @@
+import { SnapProvider } from '@metamask/snap-types';
+import { IdentitySnapState } from 'src/interfaces';
+import { switchMethod } from '../../src/rpc/did/switchMethods';
+import { connectHederaAccount } from '../../src/rpc/hedera/connectHederaAccount';
+import { getDefaultSnapState } from '../testUtils/constants';
+import { createMockWallet, WalletMock } from '../testUtils/wallet.mock';
+
+
+jest.mock('uuid');
+
+  describe('SwitchMethod', () => {
+    
+
+    let walletMock: SnapProvider & WalletMock;
+    let snapState: IdentitySnapState; 
+
+    beforeEach(async() => {
+      snapState = getDefaultSnapState();
+      walletMock = createMockWallet();
+      walletMock.rpcMocks.eth_chainId.mockReturnValue('0x128');
+
+      let privateKey = '2386d1d21644dc65d4e4b9e2242c5f155cab174916cbc46ad85622cdaeac835c';
+      let connected = await connectHederaAccount(snapState, privateKey, '0.0.15215', walletMock);
+    });
+    
+
+
+    it('should change snap state when switch to did:pkh methods', async () => {
+
+      // setup
+      snapState.accountState[snapState.currentAccount].accountConfig.identity.didMethod = "did:key";
+      walletMock.rpcMocks.snap_confirm.mockReturnValue(true);
+
+      let switchMethodResult = await switchMethod(walletMock, snapState, "did:pkh");
+      expect(switchMethodResult).toBeTruthy();
+      expect(snapState.accountState[snapState.currentAccount].accountConfig.identity.didMethod).toBe("did:pkh");
+
+      expect.assertions(2);
+    });
+
+    it('should throw error when switch to invalid method', async () => {
+
+      // setup
+      walletMock.rpcMocks.snap_confirm.mockReturnValue(true);
+
+      await expect(switchMethod(walletMock, snapState, "did:inv")).rejects.toThrowError();
+
+      expect.assertions(1);
+    });
+
+      it('should not switch method when user rejects', async () => {
+
+      snapState.accountState[snapState.currentAccount].accountConfig.identity.didMethod = "did:key";
+
+      // setup
+      walletMock.rpcMocks.snap_confirm.mockReturnValue(false);
+
+      let switchMethodPromise = switchMethod(walletMock, snapState, "did:pkh");
+
+      await expect(switchMethodPromise).resolves.toBeFalsy();
+      expect(snapState.accountState[snapState.currentAccount].accountConfig.identity.didMethod).toBe("did:key");
+
+      expect.assertions(2);
+    });
+
+
+
+
+});
