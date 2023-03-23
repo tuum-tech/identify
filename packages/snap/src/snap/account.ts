bf80a43b2b0689da85e486ce59d7388f2d205a8d
dchagastelles <dchagastelles@gmail.com>
2023-03-23 09:28:45 -0400

    fix merge error

--- packages/snap/src/snap/account.ts
+++ packages/snap/src/snap/account.ts
@@ -15,7 +15,7 @@ import { getCurrentCoinType, initAccountState } from './state';
  * Function that returns account info of the currently selected MetaMask account.
  *
  * @param state - IdentitySnapState.
- * @param account - External Account.
+ * @param account - External Account info.
  * @returns MetaMask address and did.
  */
 export async function getCurrentAccount(
@@ -41,11 +41,19 @@ export async function getCurrentAccount(
         true,
       );
     }
-
     const accounts = (await ethereum.request({
       method: 'eth_requestAccounts',
     })) as string[];
-    return await importIdentitySnapAccount(state, accounts[0]);
+    const address = accounts[0];
+    // Initialize if not there
+    const coinType = (await getCurrentCoinType()).toString();
+    if (address && !(address in state.accountState[coinType])) {
+      console.log(
+        `The address ${address} has NOT yet been configured in the Identity Snap. Configuring now...`,
+      );
+      await initAccountState(snap, state, coinType, address);
+    }
+    return await veramoImportMetaMaskAccount(snap, state, ethereum, address);
   } catch (e) {
     console.error(`Error while trying to get the account: ${e}`);
     throw new Error(`Error while trying to get the account: ${e}`);
